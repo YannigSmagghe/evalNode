ff9688bce7f536285358a54394410223e6797bbd
quentinmodena <quentinmodena@gmail.com>
2018-03-08 13:40:16 +0100

    welcomePage

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-
+app.engine('pug', require('pug').__express);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
