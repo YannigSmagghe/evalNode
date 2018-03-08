61ddb2f4c07316a713c759bd0bbcad8ef4c766f0
Yandevweb <yannick.jeanjean.pro@gmail.com>
2018-03-08 12:18:41 +0100

    Call popularity route

--- app.js
+++ app.js
@@ -22,8 +22,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Fake Popularity
+app.use(popularity);
+
 app.use('/', index);
 app.use('/users', users);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
